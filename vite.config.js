9d6a36d92a5bc2910c801bcd802b1e1b2a7d578e
Leodyver Semilla <leodyversemilla07@gmail.com>
2026-03-17 11:00:51 +0800

    fix: fix __dirname for ESM in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
     plugins: [
